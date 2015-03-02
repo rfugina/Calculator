e39d297f25be9ca801c962116e50e08532e54181
Rob Fugina <robf@fugina.com>
2015-03-01 23:38:06 -0600

    At end of first lecture

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var display: UILabel!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    var inTheMiddle: Bool = false
 
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+
+        if inTheMiddle {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            inTheMiddle = true
+        }
+        
+    }
 
 }
 
