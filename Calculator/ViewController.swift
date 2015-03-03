6aac7a3f65d1e382573f6e20201c5e06d3c561ac
Rob Fugina <robf@fugina.com>
2015-03-02 21:49:25 -0600

    Added enter and operator methods

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,19 +12,38 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
 
-    var inTheMiddle: Bool = false
+    var inNumberEntry: Bool = false
 
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
 
-        if inTheMiddle {
+        if inNumberEntry {
             display.text = display.text! + digit
         } else {
             display.text = digit
-            inTheMiddle = true
+            inNumberEntry = true
         }
-        
+
     }
 
+    @IBAction func operate(sender: UIButton) {
+    }
+    
+    var operandStack = Array<Double>()
+
+    @IBAction func enter() {
+        inNumberEntry = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+        }
+    }
 }
 
